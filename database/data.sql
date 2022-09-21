ec3ddb5b7badcc733c318523d49b382d399a73c6
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-21 00:30:07 +0000

    Update server/index.js to POST new user information to Woofles database. #12

--- database/data.sql
+++
@@ -1,4 +0,0 @@
--- dummy user until login feature is implemented
-
-INSERT INTO "users" ("userId", "email", "name", "hashedPassword")
-VALUES ('1', 'alicejeeuncodes@gmail.com', 'Alice Lee', '12345')
