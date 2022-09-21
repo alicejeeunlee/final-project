9b74bd074d656e2db5158dd61dfb84bbb329a2a2
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-21 00:05:34 +0000

    Update schema.sql and data.sql to match mock database. #12

--- database/data.sql
+++ database/data.sql
@@ -1,4 +1,4 @@
 -- dummy user until login feature is implemented
 
-INSERT INTO "users" ("userId", "username", "email", "name", "hashedPassword")
-VALUES ('1', 'alicejeeun', 'alicejeeuncodes@gmail.com', 'Alice Lee', '12345')
+INSERT INTO "users" ("userId", "email", "name", "hashedPassword")
+VALUES ('1', 'alicejeeuncodes@gmail.com', 'Alice Lee', '12345')
