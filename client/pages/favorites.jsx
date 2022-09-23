ee76ebdeead350c75ab757d1a34e2a451fd0e652
Alice Lee <alicejeeuncodes@gmail.com>
2022-09-23 22:40:34 +0000

    Mock up Favorites component rendered. #5

--- client/pages/favorites.jsx
+++ client/pages/favorites.jsx
@@ -5,15 +5,41 @@ export default class Favorites extends React.Component {
     return (
       <div className='container'>
         <div className='row'>
-          <h1 className='page-title mt-0 mb-0'>Goodest Doggos</h1>
-          <div className='list-group'>
-            <a href="" className='list-group-item list-group-item-action'>
-              <div>
-                <img src="" alt="" />
-              </div>
-              <h1>Name</h1>
-              <p>xxx miles away</p>
-            </a>
+          <div className='col'>
+            <h1 className='page-title mt-0 mb-0'>Goodest Doggos</h1>
+          </div>
+        </div>
+        <div className='row justify-content-center'>
+          <div className='col col-md-6'>
+            <div className='list-group'>
+              <a href="" className='d-flex align-items-center list-group-item list-group-item-action'>
+                <div className='circle-img-container'>
+                  <img src="/images/woofles-placeholder.png" className='favorites-img' alt="" />
+                </div>
+                <div className='ps-4'>
+                  <h1 className='favorites-name mb-0'>Name</h1>
+                  <p className='favorites-text mb-0'>xxx miles away</p>
+                </div>
+              </a>
+              <a href="" className='d-flex align-items-center list-group-item list-group-item-action'>
+                <div className='circle-img-container'>
+                  <img src="/images/woofles-placeholder.png" className='favorites-img' alt="" />
+                </div>
+                <div className='ps-4'>
+                  <h1 className='favorites-name mb-0'>Name</h1>
+                  <p className='favorites-text mb-0'>xxx miles away</p>
+                </div>
+              </a>
+              <a href="" className='d-flex align-items-center list-group-item list-group-item-action'>
+                <div className='circle-img-container'>
+                  <img src="/images/woofles-placeholder.png" className='favorites-img' alt="" />
+                </div>
+                <div className='ps-4'>
+                  <h1 className='favorites-name mb-0'>Name</h1>
+                  <p className='favorites-text mb-0'>xxx miles away</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
