53b3ddad6e9fee13f7a45f7320f2442186f0d223
Alice Lee <alicejeeuncodes@gmail.com>
2022-10-18 19:30:01 +0000

    Styled network error alert.

--- client/components/network-error.jsx
+++ client/components/network-error.jsx
@@ -1,6 +1,16 @@
+import React from 'react';
+
 export default function NetworkError(props) {
   const { error } = props;
   return (
-    alert(`${error}. Sorry, there was an error connecting to the network! Please check your internet connection and try again.`)
+    <div className="container">
+      <div className="row">
+        <div className="alert alert-info justify-content-" role="alert">
+          <h1>Network Error</h1>
+          <p>Response from server: {error}.</p>
+          <p>Sorry, there was an error connecting to the network! Please check your internet connection and try again.</p>
+        </div>
+      </div>
+    </div>
   );
 }
